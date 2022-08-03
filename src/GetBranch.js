53b408a3ed7a81dad336ecea786cc7f47b053183
SVS <stepanezc@gmail.com>
2022-08-03 15:56:59 +0300

    Test action

---
+++ src/GetBranch.js
@@ -0,0 +1,57 @@
+﻿const github = require('@actions/github');
+
+// use an async function for the main tasks
+class CreateBranch{
+    constructor() {
+        this.inputs = {};
+    };
+
+    setup(inputs) {
+        for(const [key, value] of Object.entries(inputs)) {
+            this.inputs[key] = value;
+        };
+        this.octokit = github.getOctokit(this.inputs.GITHUB_TKN);
+    };
+
+    setLogger({notice, info, output, warning, error}) {
+        this.notice = notice;
+        this.info = info;
+        this.output = output;
+        this.warning = warning;
+        this.error = error;
+    };
+
+    async run() {
+        try {
+
+        } catch (error) {
+
+        }
+    };
+
+    async CreateBranch() {
+        try {
+
+            const owner = this.inputs.OWNER;
+            const repo =  this.inputs.REPO;
+            const mainBranch = this.inputs.MAIN_BRANCH;
+
+            let MainBranchSHA = await this.octokit.request("GET /repos/{owner}/{repo}/git/refs/{ref}", {
+                owner: owner,
+                repo: repo,
+                ref: `heads/${mainBranch}`
+            });
+
+            let SHA = [];
+            MainBranchSHA.data.forEach(element => {
+                SHA.push(element.object.sha)
+            });
+            return SHA
+
+        } catch (error) {
+            throw error;
+        }
+    };
+}
+
+module.exports = CreateBranch;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+﻿const core = require("@actions/core");
+const CreateBranch = require("./GetBranch");
+
+(async () =>{
+    try {
+        const inputs = {
+            REPO: core.getInput("repo").trim(),
+            OWNER: core.getInput("owner").trim(),
+            GITHUB_TKN: core.getInput("github_tkn").trim(),
+            MAIN_BRANCH: core.getInput("main_branch").trim(),
+            TARGET_BRANCH: core.getInput("target_branch").trim()
+        };
+
+        const actionOcto = new CreateBranch();
+        actionOcto.setup(inputs);
+        actionOcto.setLogger({
+            notice: core.notice,
+            info: core.info,
+            output: core.setOutput,
+            warning: core.warning,
+            error: core.error,
+        });
+
+        await actionOcto.run();
+
+    } catch (error) {
+        console.error(error);
+        core.setFailed(error);
+        throw error;
+    }
+})();
