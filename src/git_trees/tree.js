7b0ed79d9f149c5f8808decc9a56c91c309aef95
SVS <stepanezc@gmail.com>
2022-08-19 21:43:32 +0300

    Update

--- src/git_trees/tree.js
+++ src/git_trees/tree.js
@@ -10,18 +10,21 @@ class GitTree{
     };
 
     async CreateTree(BlobList, message, trunk) {
-        core.warning(`Tree creation process started`);
+        core.warning(`Tree creation process started:`);
+        core.warning("Tree creation: Get main branch name");
         const MainBranchName = await this.octokit.request("GET /repos/{owner}/{repo}", {
             owner: this.owner,
             repo: this.repo
         });
 
+        core.warning("Tree creation: Get main branch SHA");
         const MainBranchSHA = await this.octokit.request("GET /repos/{owner}/{repo}/git/refs/{ref}", {
             owner: this.owner,
             repo: this.repo,
             ref: `heads/${MainBranchName.data.default_branch}`
         });
 
+        core.warning("Defining tree structure");
         this.tree = await this.octokit.request("POST /repos/{owner}/{repo}/git/trees", {
             owner: this.owner,
             repo: this.repo,
@@ -29,14 +32,17 @@ class GitTree{
             tree: BlobList
         });
 
+        core.warning(`Create commit; Tree status from prev: ${this.tree.status}`);
         const commit = await this.octokit.request("POST /repos/{owner}/{repo}/git/commits", {
             owner: this.owner,
             repo: this.repo,
             message: message,
-            tree: this.tree.sha,
-            parents: [MainBranchSHA.data.object.sha,]
+            tree: this.tree.sha //,
+            // parents: [MainBranchSHA.data.object.sha]
         });
         core.warning(`Commit for tree: ${commit.data.sha}`);
+
+        core.warning(`Updating ref`);
         await this.octokit.request("PATCH /repos/{owner}/{repo}/git/refs/{ref}", {
             owner: this.owner,
             repo: this.repo,
